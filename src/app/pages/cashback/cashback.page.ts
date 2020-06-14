43ab0c73561b2dc7ff33c51f1e5cec3047fc5384
Daniela Cardoso Da Costa <dccosta2@latam.stefanini.com>
2020-06-14 12:08:56 -0300

    Correção leitura QR Code

--- src/app/pages/cashback/cashback.page.ts
+++ src/app/pages/cashback/cashback.page.ts
@@ -18,6 +18,7 @@ export class CashbackPage extends Utils implements OnInit {
     { tipoCashback: 'a179aa0c-3802-4653-88f1-e8e7ce6c9037', descricao: 'Cadastro efetivado com sucesso!', valor: 50 },
     { tipoCashback: '832eb07e-0338-4e28-9617-1bd87ebd64a7', descricao: 'Consulta médica no posto CCR', valor: 100 },
     { tipoCashback: 'ec5b0505-5194-46bc-8a94-7902fc168654', descricao: 'Banho', valor: -40 },
+    { tipoCashback: '3c9fd4f3-e7bf-42ed-9edf-b5d094a83e08', descricao: 'Limpeza do caminhão', valor: -80 },
   ];
   pontuacaoAtual = 0;
 
@@ -29,14 +30,13 @@ export class CashbackPage extends Utils implements OnInit {
   }
 
   ngOnInit() {
-    //Obter leituras e pontuacao atual do caminhoneiro
+    // Obter leituras e pontuacao atual do caminhoneiro
     this.leituras = [
-      { tipoCashback: 'ec5b0505-5194-46bc-8a94-7902fc168654', descricao: 'Banho', valor: -40, data: new Date() },
-      { tipoCashback: 'a179aa0c-3802-4653-88f1-e8e7ce6c9037', descricao: 'Cadastro efetivado com sucesso!', valor: 50, data: new Date() },
+      { tipoCashback: 'a179aa0c-3802-4653-88f1-e8e7ce6c9037', descricao: 'Cadastro efetivado com sucesso!', valor: 50 },
     ];
 
     this.leituras.forEach(item => {
-      this.pontuacaoAtual += item.valor
+      this.pontuacaoAtual += item.valor;
     });
   }
 
@@ -44,11 +44,10 @@ export class CashbackPage extends Utils implements OnInit {
     this.barcodeScanner.scan().then(
       scan => {
         this.scannedCode = scan.text;
-        this.exibirMensagem('Scan!', scan.text);
         this.cashbackLido = JSON.parse(this.scannedCode);
         if (this.cashbackLido && this.cashbackLido.tipoCashback) {
-          this.tipoCashback = this.tipoCashback.filter(x => x.tipoCashback == this.cashbackLido.tipoCashback);
-          this.exibirMensagem('Cashback', this.tipoCashback);
+          this.tipoCashback = this.tiposCashback.find(x => x.tipoCashback === this.cashbackLido.tipoCashback);
+          this.incluirLeituraLista();
         } else {
           this.exibirMensagem('Ops!', 'O QR não é compatível com o aplicativo.');
         }
@@ -63,7 +62,16 @@ export class CashbackPage extends Utils implements OnInit {
   }
 
   incluirLeituraLista() {
-    this.leituras.unshift(this.scannedCode);
-    // Persistir nova leitura
+    const verificacaoSaldo = this.pontuacaoAtual + this.tipoCashback.valor;
+    if (verificacaoSaldo >= 0) {
+      this.leituras.unshift(this.tipoCashback);
+      this.leituras = this.leituras.slice(0, 4);
+      this.pontuacaoAtual += this.tipoCashback.valor;
+
+      // Persistir nova leitura
+    } else {
+      this.exibirMensagem('Ops!', 'Você não possui saldo suficiente para essa ação.');
+    }
+
   }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -10,11 +10,11 @@ import { Utils } from '../utils';
 export class HomePage extends Utils implements OnInit {
 
   cards: any[] = [
+    { titulo: 'Saúde e bem-estar', destino: 'saude-bem-estar', descricao: 'Lembre-se: saúde em primeiro lugar!', icone: 'fitness-outline' },
     { titulo: 'Rotas', destino: 'rotas', descricao: 'Pegue sempre o melhor caminho para chegar ao seu destino!', icone: 'map-outline' },
     { titulo: 'Postos CCR', destino: 'postos-ccr', descricao: 'Saiba onde encontrar os postos da CCR!', icone: 'pin' },
     { titulo: 'Paradas', destino: 'paradas', descricao: 'Que tal um banho quente? Ou uma refeição bem gostosa?', icone: 'cafe-outline' },
     { titulo: 'Postos de Combustíveis', destino: 'postos-combustivel', descricao: 'Saiba onde abastecer seu caminhão e evite contratempos!', icone: 'speedometer-outline' },
-    { titulo: 'Saúde e bem-estar', destino: 'saude-bem-estar', descricao: 'Lembre-se: saúde em primeiro lugar!', icone: 'fitness-outline' },
     { titulo: 'Emergências', destino: 'emergencia', descricao: 'Não hesite em pedir ajuda!', icone: 'call-outline' },
     { titulo: 'Comunicados', destino: 'comunicados', descricao: 'Fique sempre por dentro do que acontece na CCR!', icone: 'megaphone-outline', cor: 'red' },
     { titulo: 'Notícias', destino: 'noticias', descricao: 'O que acontece no Brasil e no mundo!', icone: 'alert-circle-outline' },
